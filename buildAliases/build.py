a92155be9b6414bfd5267c78f93f9264831a5816
PanDoes <pandasbuisness@gmail.com>
2017-09-06 16:40:49 -0700

    name not names

--- buildAliases/build.py
+++ buildAliases/build.py
@@ -2,5 +2,5 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome()
 browser.get("https://minecraft.curseforge.com/mc-mods")
-modlist = browser.find_elements_by_class_names("name-wrapper overflow-tip")
+modlist = browser.find_elements_by_class_name("name-wrapper overflow-tip")
 driver.close()
