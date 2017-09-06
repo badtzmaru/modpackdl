09451a655e12185056f339e8ba628d304b32c257
Badtz Maru <badtzmaru139@gmail.com>
2017-09-06 16:31:01 -0700

    function to download mods based on project ids

---
+++ dl.py
@@ -0,0 +1,24 @@
+import os, requests
+from   bs4 import BeautifulSoup
+
+def downloadMod(modId):
+    with requests.session() as s:
+        s.headers['user-agent'] = 'Mozilla/5.0'
+
+        r = s.get('https://minecraft.curseforge.com/projects/' + str(modId))
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        url  = soup.find('a', 'button alt fa-icon-download')['href']
+
+        modName = soup.find('span', 'overflow-tip').string + '.jar'
+
+        url = 'https://minecraft.curseforge.com/' + url
+        print("Now downloading: " + str(modName))
+
+        r = s.get(url, stream=True)
+
+        with open(os.path.basename(modName), 'wb') as zipfile:
+            for chunk in r.iter_content(chunk_size=1024):
+                zipfile.write(chunk)
+
+downloadMod(32274)
