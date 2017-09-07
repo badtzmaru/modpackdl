043e9802e1be3aa3ae4486103c5ead2daef5493a
Badtz Maru <badtzmaru139@gmail.com>
2017-09-06 18:12:32 -0700

    update csv write

--- buildAliases/build.py
+++ buildAliases/build.py
@@ -3,16 +3,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import csv
 import os
+import sys
 
 chromeOptions = Options()
-chromeOptions.add_argument("--kiosk")
+#chromeOptions.add_argument("--kiosk")
 browser = webdriver.Chrome(chrome_options=chromeOptions)
 #browser.get("https://minecraft.curseforge.com/mc-mods")
 #gameVerSelect = browser.find_element_by_id("filter-game-version")
 #gameVerOption = browser.find_elements_by_class_name("game-version-type")[0]
 #gameVerSelect.click()
 #gameVerOption.click()
-for x in range(0, 9):
+if(len(sys.argv) != 2):
+    pages = 50
+modlist = []
+for x in range(0, sys.argv[1]):
     pageUrl = "https://minecraft.curseforge.com/mc-mods?filter-game-version=1738749986:628&filter-sort=popularity&page=" + str(x)
     for y in range(0, 19):
         browser.get(pageUrl)
@@ -20,6 +24,7 @@ for x in range(0, 9):
         modList[y].click()
         modName = browser.find_elements_by_class_name("overflow-tip")[0].text
         modId = browser.find_elements_by_class_name("info-data")[0].text
-        with open('aliases.csv','wb') as file:
-            file.write(modName + ", " + modId + "\n")
+        modlist.append(str(modName) + "," + str(modId) + "\n")
+with open('aliases.csv','w') as file:
+    file.writelines(modlist)
 browser.close()
