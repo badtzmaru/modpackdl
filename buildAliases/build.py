706c165eefa5639bf9dff638530e218b789290df
PanDoes <pandasbuisness@gmail.com>
2017-09-06 21:35:54 -0700

    Improved Build Speed
    +19 pages of Aliases

--- buildAliases/build.py
+++ buildAliases/build.py
@@ -12,26 +12,33 @@ try:
     os.remove("aliases.csv")
 except OSError:
     pass
-#browser.get("https://minecraft.curseforge.com/mc-mods")
+browser.get("https://minecraft.curseforge.com/mc-mods")
 #gameVerSelect = browser.find_element_by_id("filter-game-version")
 #gameVerOption = browser.find_elements_by_class_name("game-version-type")[0]
 #gameVerSelect.click()
 #gameVerOption.click()
+parentElement = browser.find_elements_by_class_name("overflow-tip")[0]
+elementList = parentElement.find_element_by_tag_name("a").get_attribute("href")
+
 if(len(sys.argv) != 2):
     pages = 50
 else:
     pages = int(sys.argv[1])
-modlist = []
+modList = []
+modComplete = []
 for x in range(0, pages):
     pageUrl = "https://minecraft.curseforge.com/mc-mods?filter-game-version=1738749986:628&filter-sort=popularity&page=" + str(x+1)
+    browser.get(pageUrl)
     for y in range(0, 20):
-        browser.get(pageUrl)
-        modList = browser.find_elements_by_class_name("overflow-tip")
-        modList[y].click()
-        modName = browser.find_elements_by_class_name("overflow-tip")[0].text
-        modId = browser.find_elements_by_class_name("info-data")[0].text
-        modlist.append(str(modName).encode('utf-8') + "," + str(modId) + "\n")
-modlist.sort()
+        parentElement = browser.find_elements_by_class_name("overflow-tip")[y]
+        modList.append(parentElement.find_element_by_tag_name("a").get_attribute("href"))
+for i in range(0, len(modList)):
+    browser.get(modList[i])
+    modName = browser.find_elements_by_class_name("overflow-tip")[0].text
+    modId = browser.find_elements_by_class_name("info-data")[0].text
+    modComplete.append(str(modName).encode('utf-8') + "," + str(modId) + "\n")
+modComplete.sort()
 with open('aliases.csv','w') as file:
-    file.writelines(modlist)
+   file.writelines(modComplete)
 browser.close()
+
