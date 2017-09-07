87db77c771d8be5d2e38ba860ea2a6f39529686a
PanDoes <pandasbuisness@gmail.com>
2017-09-06 17:03:56 -0700

    Got name, working on getting ID

--- buildAliases/build.py
+++ buildAliases/build.py
@@ -2,5 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome()
 browser.get("https://minecraft.curseforge.com/mc-mods")
-modlist = browser.find_elements_by_class_name("overflow-tip")
+modList = browser.find_elements_by_class_name("overflow-tip")
+modList[0].click()
+modName = browser.find_element_by_class_name("overflow-tip").text
+modId = browser.find_elements_by_class_name("info-data")
+print(modName)
+print(modId[0])
 browser.close()
