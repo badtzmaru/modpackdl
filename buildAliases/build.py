d903989d69e3f1bfd38ffbb92ce2cd0c8031518c
Badtz Maru <badtzmaru139@gmail.com>
2017-09-12 17:28:28 -0700

    bs4 is done

--- buildAliases/build.py
+++ buildAliases/build.py
@@ -1,5 +1,5 @@
 #import dependencies
-import csv, os, sys, requests
+import csv, os, sys, requests, time
 import progressbar
 from bs4 import BeautifulSoup
 
@@ -36,6 +36,8 @@ pageBar = progressbar.ProgressBar()
 modBar = progressbar.ProgressBar()
 modList = []
 
+start = time.time()
+
 with requests.session() as browser:
     browser.headers['user-agent'] = 'Mozilla/5.0'
 
@@ -43,19 +45,20 @@ with requests.session() as browser:
         pageUrl = "https://minecraft.curseforge.com/mc-mods?filter-game-version=1738749986:628&filter-sort=popularity&page=" + str(x+1)
         r = browser.get(pageUrl)
         soup = BeautifulSoup(r.text, 'html.parser')
-        parentElement = soup.find('div', {'class': 'overflow-tip'})
-        children = parentElement.findChildren()
-        for y in range(0,len(children)): 
-            modList.append(children[y]['href'])
+        parentElement = soup.find_all('div', {'class': 'overflow-tip'})
+        for y in range(0,len(parentElement)):
+            children = parentElement[y].findChildren()
+            for z in range(0, len(children)):
+                modList.append(children[z]['href'])
 
+    print(str(len(modList)) + " mods found!")
     print("Getting project ids...")
     for i in modBar(range(0, len(modList))):
         r = browser.get("https://minecraft.curseforge.com" + modList[i])
         soup = BeautifulSoup(r.text, 'html.parser')
         modName = soup.find('span', {'class': 'overflow-tip'}).string
         modId = soup.find('div', {'class': 'info-data'}).string
-        print(modName)
-        print(modId)
         addMod(str(modName.encode('utf-8')) + "," + str(modId) + "\n")
 
-    print("Build complete!")
+    end = time.time()
+    print("Build completed in " + str(end-start))
